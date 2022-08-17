613b97c964c60145f828aa8578d0eb8197395675
Grace Bunardi <gbunardi@hotmail.com>
2022-08-17 11:46:49 +0800

    update & delete complete, pending refactoring and front-end

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
 
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
   post 'tasks', to: 'tasks#create'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
